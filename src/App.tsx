fa8e3a7ddf1d2875d511d8f42e81d8ce428447b4
perfume <perfumelim@gmail.com>
2022-06-09 14:22:49 +0900

    ✨ React-hook-form으로 간단한 form 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,48 @@
 import React from "react";
-import "./App.css";
-import Slider from "react-slick";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Counter from "./components/count";
-import LottieBox from "./components/LottieBox";
 
-export interface IArrow {
-  className?: string | undefined;
-  style?: React.CSSProperties | undefined;
-  onClick?: React.MouseEventHandler<any> | undefined;
-}
+type Inputs = {
+twitterUsername: string,
+userToken: string,
 
-function App() {
-  function SampleNextArrow(props: IArrow) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "black" }}
-        onClick={onClick}
-      />
-    );
-  }
+};
 
-  function SamplePrevArrow(props: IArrow) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "green" }}
-        onClick={onClick}
-      />
-    );
-  }
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-  };
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>();
+
   return (
-    <Container>
-      <h2> Single Item</h2>
-      <Counter />
-      <LottieBox />
-      <StyledSlider {...settings}>
-        <div>
-          <img src="http://placekitten.com/g/400/200" alt="" />
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-      </StyledSlider>
+<Container>
+  <h1>TEST</h1>
+  <form
+      onSubmit={handleSubmit((data) => {
+        alert(JSON.stringify(data));
+      })}
+    >
+      <label>Example</label>
+      <input {...register("twitterUsername", {
+        required: true,
+        minLength: 4,
+      })} type="text" id="username"/>
+      {errors.twitterUsername?.type === 'required' && "Please Enter your Twitter Username"}
+      <label>ExampleRequired</label>
+      <input
+        {...register("userToken", { required: true, maxLength: 10 })}
+      />
+      {errors.userToken && <p>This field is required</p>}
+      <input type="submit" />
+    </form>
     </Container>
   );
 }
-
-export default App;
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   padding: 50px;
   background-color: ;
-`;
-
-const StyledSlider = styled(Slider)`
-  width: 500px;
 `;
