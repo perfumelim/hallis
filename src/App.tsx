7ea41f2c8e5afe1995fd91c2e2b2b2ed5c0220a4
perfume <perfumelim@gmail.com>
2022-06-09 18:14:45 +0900

    💄 Global Style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import Login from "./components/Login";
+import Login from "components/Login";
+import GlobalStyles from "styles/GlobalStyles";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
       <Login />
     </QueryClientProvider>
   );
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    html,
+    body,
+    #root {
+    height: 100%;
+    }
+    * {
+    box-sizing: border-box;
+    }
+    body {
+        font-family: 'Noto Sans KR', sans-serif;
+        }
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
+    button {
+        background: inherit;
+        border: none;
+        box-shadow:none;
+    }
+    ol,ul,li {
+        list-style: none;
+    }
+`;
+
+export default GlobalStyles;
