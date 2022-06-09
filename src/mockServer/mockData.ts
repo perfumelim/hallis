af72475b8e8b0b569934be83d6a51b81607b4f9b
perfume <perfumelim@gmail.com>
2022-06-09 18:00:50 +0900

    mirage JS로 Mock server

---
+++ src/mockServer/mockData.ts
@@ -0,0 +1,13 @@
+const mockData = {
+  code: [
+    { id: "abc123" },
+    { id: "def123" },
+    { id: "def345" },
+    { id: "def567" },
+    { id: "def890" },
+    { id: "abc456" },
+    { id: "def888" },
+  ],
+};
+
+export default mockData;
