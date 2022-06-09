76a04ec217885785340527234734890c661ced3e
perfume <perfumelim@gmail.com>
2022-06-09 18:01:25 +0900

    🔥 불필요한 파일 및 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,13 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import styled from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Login from "./components/Login";
 
-type Inputs = {
-twitterUsername: string,
-userToken: string,
-
-};
+const queryClient = new QueryClient();
 
 export default function App() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<Inputs>();
-
   return (
-<Container>
-  <h1>TEST</h1>
-  <form
-      onSubmit={handleSubmit((data) => {
-        alert(JSON.stringify(data));
-      })}
-    >
-      <label>Example</label>
-      <input {...register("twitterUsername", {
-        required: true,
-        minLength: 4,
-      })} type="text" id="username"/>
-      {errors.twitterUsername?.type === 'required' && "Please Enter your Twitter Username"}
-      <label>ExampleRequired</label>
-      <input
-        {...register("userToken", { required: true, maxLength: 10 })}
-      />
-      {errors.userToken && <p>This field is required</p>}
-      <input type="submit" />
-    </form>
-    </Container>
+    <QueryClientProvider client={queryClient}>
+      <Login />
+    </QueryClientProvider>
   );
 }
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  padding: 50px;
-  background-color: ;
-`;
