922695be148bb05407eb6e728ff62e1b4578295b
perfume <perfumelim@gmail.com>
2022-06-10 10:52:06 +0900

    mirage -> jsonserver

--- src/hooks/useCodeQuery.ts
+++ src/hooks/useCodeQuery.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default function useCodeQuery() {
   const { data, isLoading, isError } = useQuery("codedata", () =>
-    axios.get("http://localhost:8000/code").then((res) => res.data)
+    axios.get("http://localhost:4000/code").then((res) => res.data)
   );
 
   return {
