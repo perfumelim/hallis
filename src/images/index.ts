dba75ca4bf189083aceded35a2ccd8b22bb4065b
perfume <perfumelim@gmail.com>
2022-06-13 16:39:42 +0900

    🌱 이미지 파일 추가

---
+++ src/images/index.ts
@@ -0,0 +1,13 @@
+import vampire from "images/Vampire.png";
+import logo from "images/logo.png";
+import twitterIcon from "images/twitter_icon.svg";
+import discordIcon from "images/discord_icon.svg";
+import mousaiLogo from "images/mousai_logo.svg";
+
+export const images = {
+  vampire,
+  logo,
+  twitterIcon,
+  discordIcon,
+  mousaiLogo,
+};
