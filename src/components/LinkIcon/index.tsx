c9e35c5d0064026c4c4ea1081a2c9aa1320ca5ac
perfume <perfumelim@gmail.com>
2022-06-15 11:36:47 +0900

    ✨ link 이동 추가

--- src/components/LinkIcon/index.tsx
+++ src/components/LinkIcon/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { images } from "images/index";
 import style from "./style";
-const snsIcons = [images.twitterIcon, images.discordIcon];
 const { Wrapper, Icon } = style;
+
+const IconGroups = [
+  { id: 1, img: images.twitterIcon, url: "https://twitter.com/mousai_club" },
+  { id: 2, img: images.discordIcon, url: "https://discord.gg/mc56HHPFKu" },
+];
+
 export default function LinkIcon() {
   return (
     <Wrapper>
-      {snsIcons.map((icon, idx) => {
-        return <Icon key={idx} img={icon}></Icon>;
+      {IconGroups.map((data) => {
+        return (
+          <a href={data.url}>
+            <Icon key={data.id} img={data.img}></Icon>
+          </a>
+        );
       })}
     </Wrapper>
   );
