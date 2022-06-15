7a3f85b0d2071eef6590f7066c4605f8e4b42d75
perfume <perfumelim@gmail.com>
2022-06-15 18:38:47 +0900

    💄 아이콘 추가

--- src/images/index.ts
+++ src/images/index.ts
@@ -3,11 +3,13 @@ import logo from "images/logo.png";
 import twitterIcon from "images/twitter_icon.svg";
 import discordIcon from "images/discord_icon.png";
 import mousaiLogo from "images/mousai_logo.svg";
+import instaIcon from "images/insta_icon.svg";
 
 export const images = {
   vampire,
   logo,
   twitterIcon,
   discordIcon,
+  instaIcon,
   mousaiLogo,
 };
